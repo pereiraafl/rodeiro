e8a4bce5abb0b309c7b6b8676b5e935baa5205a8
Lucas Bernardino <lucasgber@gmail.com>
2024-10-18 16:37:39 -0300

    Add url body parser and route to get all data from DB

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import dotenv from "dotenv";
 
 import { Rodeiro } from "./schema";
+import { IRodeiro } from './models';
 
 dotenv.config();
 
@@ -11,19 +12,37 @@ const connectionString = process.env.DB || "";
 
 const app = express();
 
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }));
+
 const port: number = 3000;
 
-app.get('/', async (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
 
-  const rodeiro = new Rodeiro({
-    temp_init: 55,
-    temp_final: 78,
-    cycle: 1
-  });
+  res.json({ "server": "working fine" });
 
-  const newUser = await rodeiro.save();
-  res.status(201).json(newUser);
+})
 
+app.post('/send', async (req: Request, res: Response) => {
+  try {
+    const rodeiro_body: IRodeiro = req.body;
+    const rodeiro = new Rodeiro(rodeiro_body);
+    await rodeiro.save();
+    res.status(201);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ "error": "failed to parse 'rodeiro' struct" });
+  }
+});
+
+app.get('/get', async (req: Request, res: Response) => {
+  try {
+    const data = await Rodeiro.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ "error": "failed to get the users" });
+  }
 });
 
 app.listen(port, async () => {
