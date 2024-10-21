24fe259fd9ce6bc51d993a67fe1d5590f1ed8a4d
Lucas Bernardino <lucasgber@gmail.com>
2024-10-21 09:44:18 -0300

    create post and get routes for testing both highestlowest and continuous

--- api/test.py
+++ api/test.py
@@ -2,13 +2,32 @@ import requests
 
 rota = "http://localhost:3000"
 
-def create():
+def create_highestlowest():
     rodeiro = {
-        "temp_init": 30,
-        "temp_final": 50,
+        "temp_init": 4,
+        "temp_final": 20,
         "cycle": 2
     }
-    req = requests.post(f"{rota}/send", json=rodeiro)
+    req = requests.post(f"{rota}/highestlowest", json=rodeiro)
     print(req)
 
-print(requests.get(f"{rota}/get").text)
+def create_continuous():
+    rodeiro = {
+        "current_temp": 69,
+        "cycle": 2
+    }
+    req = requests.post(f"{rota}/continuous", json=rodeiro)
+    print(req)
+
+def get_highestlowest():
+    req = requests.get(f"{rota}/highestlowest")
+    print(req.text)
+
+def get_continuous():
+    req = requests.get(f"{rota}/continuous")
+    print(req.text)
+
+# create_highestlowest()
+# create_continuous()
+# get_highestlowest()
+# get_continuous()
