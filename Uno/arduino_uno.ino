d4c7a0ea4fe1da89b361ae44a932a8111fecc952
Lucas Bernardino <lucasgber@gmail.com>
2024-11-21 15:21:21 -0300

    Update latest version of arduino uno code

--- Uno/arduino_uno.ino
+++ Uno/arduino_uno.ino
@@ -11,6 +11,11 @@ int thermoD0 = 3; // so
 int thermoCS = 4;
 int thermoCLK = 5; // sck
 float temp;
+
+String pythonMessage = "";
+bool rodeiroStatus = true;
+bool doOnce = true;
+
 void setup() {
   Serial.begin(9600); // Iniciar leitura serial, definindo baudrate = 9600
   // Definindo todos os pinos de relé como OUTPUT (eles terão a função de enviar informação ao relé)
@@ -27,106 +32,86 @@ void setup() {
   lcd.print("INICIANDO..."); // Escrever na tela que está iniciando
 }
 void loop() {
-    // Deixar ambos os comandos da central em seu estado padrão:
-    digitalWrite(acionar_k1_pin, HIGH); // Freio não acionado
-    digitalWrite(liberar_k2_pin, LOW); // Válvula comutada permitindo passagemdo fluido (para permitir que a pastilha volte mais facilmente diminuindo aresistência)
-    int value = analogRead(A0); // Ler valor da tensão enviada pelo relé interno do inversor (deve estar em 5V até atingir a velocidade desejada, definida em P281 e P002)
-    float voltage = value*(5.0/1023.0); // Converter o valor codificado em tensão real
-    //Serial.println(voltage); // Mostrar a tensão no leitor serial
-    if(voltage < 4.7){ // Se a tensão estiver abaixo de 4,90 (compensa pela flutuação entre 5 e 4,90), então terá atingido a velocidade desejada e a frenagem deve ser iniciada
-        if(ciclo == 0){ // Se estiver no primeiro ciclo, esperar por 5 min (300 s ou 300000 ms) para aquecer os rolamentos
-            // Mostrando na tela que está no período de aquecimento
-            lcd.clear();
-            lcd.setCursor(0,0);
-            lcd.print("AQUECIMENTO");
-            lcd.setCursor(0,1);
-            lcd.print("Ciclo: ");
-            lcd.setCursor(8,1);
-            lcd.print(ciclo + 1);
-            // Espera por 5 min
-            delay(3000);
-        }
-        digitalWrite(inversor_pin, HIGH); // Desliga o motor comandando o relé do inversor
-        
-        
-        // Mostra a temperatura e o ciclo atual
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("Temp: ");
-        lcd.setCursor(7,0);
-        lcd.print(temp);
-        lcd.setCursor(0,1);
-        lcd.print("Ciclo: ");
-        lcd.setCursor(8,1);
-        lcd.print(ciclo + 1);
-
-        delay(500);
-        // Aciona o freio comandando o relé da válvula da central para impedir a    liberação de pressão e comandando o relé de acionamento da central
-        digitalWrite(liberar_k2_pin, HIGH);
-        delay(500); // Esperar um pouco antes de acionar
-        digitalWrite(acionar_k1_pin, LOW);
-        //Serial.println("Central acionada");
-        delay(500); // Esperar meio segundo antes de tirar o acionamento (não énecessário muito tempo para que a pressão seja atingida)
-        // Parar o acionamento (pressão será mantida)
-        digitalWrite(acionar_k1_pin, HIGH);
-
-        long time_before_while = millis();
-
-        //TODO: Enviar sinal para PC dizendo que precisa ler a temperatura minima.
-
-        Serial.println("Start");
-
-        // Aguardar 8 segundos (dentro do while) para a frenagem ser realizada.
-        while ((millis() - time_before_while) < 8000) {
-            lcd.clear();
-            lcd.setCursor(0,0);
-            lcd.print("Temp: ");
-            lcd.setCursor(7,0);
-            lcd.print(temp);
-            lcd.setCursor(0,1);
-            lcd.print("Ciclo: ");
-            lcd.setCursor(8,1);
-            lcd.print(ciclo + 1);
-            delay(150);
-        }
-
-        
-        // Liberar a pressão (voltar ao estado padrão) comandando o relé que comanda a válvula da central
-        digitalWrite(liberar_k2_pin, LOW);
-
-        time_before_while = millis();
-
-        // Aguardar mais 8 segundos dentro do while para garantir liberação total da pressão e reiniciar o ciclo
-        while ((millis() - time_before_while) < 8000) {
-            lcd.clear();
-            lcd.setCursor(0,0);
-            lcd.print("Temp: ");
-            lcd.setCursor(7,0);
-            lcd.print(temp);
-            lcd.setCursor(0,1);
-            lcd.print("Ciclo: ");
-            lcd.setCursor(8,1);
-            lcd.print(ciclo + 1);
-            delay(150);
-        }
-
-        //TODO: Enviar sinal para PC dizendo que a ultima leitura lida foi a máxima.
-
-        Serial.println("End");
-
-        ciclo = ciclo + 1; // Acrescentar à contagem de ciclos
-        } else { // Se não tiver atingido a velocidade, manter o motor ligado até atingir
-        digitalWrite(inversor_pin, LOW);
-        // Mostra a temperatura atual (neste caso, em tempo real) e o ciclo atual
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("Temp: ");
-        lcd.setCursor(7,0);
-        lcd.print(temp);
-        lcd.setCursor(0,1);
-        lcd.print("Ciclo: ");
-        lcd.setCursor(8,1);
-        lcd.print(ciclo + 1);
+  
+    if (Serial.available()) {
+      pythonMessage = Serial.readString();
+      if (pythonMessage.startsWith("on")) {
+        rodeiroStatus = true;
+      }
+      if (pythonMessage.startsWith("off")) {
+        rodeiroStatus = false;
+        doOnce = true;
+      }
     }
-    delay(200); // Tempo necessário para leitura adequada do termopar
+
+    if (rodeiroStatus) {
+      // Deixar ambos os comandos da central em seu estado padrão:
+      digitalWrite(acionar_k1_pin, HIGH); // Freio não acionado
+      digitalWrite(liberar_k2_pin, LOW); // Válvula comutada permitindo passagemdo fluido (para permitir que a pastilha volte mais facilmente diminuindo aresistência)
+      int value = analogRead(A0); // Ler valor da tensão enviada pelo relé interno do inversor (deve estar em 5V até atingir a velocidade desejada, definida em P281 e P002)
+      float voltage = value*(5.0/1023.0); // Converter o valor codificado em tensão real
+      if(voltage < 4.5){ // Se a tensão estiver abaixo de 4,90 (compensa pela flutuação entre 5 e 4,90), então terá atingido a velocidade desejada e a frenagem deve ser iniciada
+          // if(ciclo == 0){ // Se estiver no primeiro ciclo, esperar por 5 min (300 s ou 300000 ms) para aquecer os rolamentos
+          //     // Mostrando na tela que está no período de aquecimento
+          //     // Espera por 5 min
+          //     delay(3000);
+          // }
+          digitalWrite(inversor_pin, HIGH); // Desliga o motor comandando o relé do inversor
+
+          delay(100);
+          // Aciona o freio comandando o relé da válvula da central para impedir a    liberação de pressão e comandando o relé de acionamento da central
+          digitalWrite(liberar_k2_pin, HIGH);
+          delay(100); // Esperar um pouco antes de acionar
+          digitalWrite(acionar_k1_pin, LOW);
+          //Serial.println("Central acionada");
+          delay(500); // Esperar meio segundo antes de tirar o acionamento (não énecessário muito tempo para que a pressão seja atingida)
+          // Parar o acionamento (pressão será mantida)
+          digitalWrite(acionar_k1_pin, HIGH);
+
+          long time_before_while = millis();
+
+          //TODO: Enviar sinal para PC dizendo que precisa ler a temperatura minima.
+
+          Serial.println("Start");
+
+          // Aguardar 8 segundos (dentro do while) para a frenagem ser realizada.
+          while ((millis() - time_before_while) < 5000) {
+              delay(150);
+          }
+
+          
+          // Liberar a pressão (voltar ao estado padrão) comandando o relé que comanda a válvula da central
+          digitalWrite(liberar_k2_pin, LOW);
+
+          time_before_while = millis();
+
+          // Aguardar mais 8 segundos dentro do while para garantir liberação total da pressão e reiniciar o ciclo
+          while ((millis() - time_before_while) < 6000) {
+              delay(150);
+          }
+
+          //TODO: Enviar sinal para PC dizendo que a ultima leitura lida foi a máxima.
+
+          Serial.println("End");
+
+          ciclo = ciclo + 1; // Acrescentar à contagem de ciclos
+          } else { // Se não tiver atingido a velocidade, manter o motor ligado até atingir
+          digitalWrite(inversor_pin, LOW);
+          // Mostra a temperatura atual (neste caso, em tempo real) e o ciclo atual
+      }
+      delay(200); // Tempo necessário para leitura adequada do termopar
+    } 
+    if (!rodeiroStatus && doOnce) {
+      digitalWrite(inversor_pin, HIGH); // Desliga o motor comandando o relé do inversor
+      delay(500);
+      // Aciona o freio comandando o relé da válvula da central para impedir a    liberação de pressão e comandando o relé de acionamento da central
+      digitalWrite(liberar_k2_pin, HIGH);
+      delay(500); // Esperar um pouco antes de acionar
+      digitalWrite(acionar_k1_pin, LOW);
+      //Serial.println("Central acionada");
+      delay(500); // Esperar meio segundo antes de tirar o acionamento (não énecessário muito tempo para que a pressão seja atingida)
+      // Parar o acionamento (pressão será mantida)
+      digitalWrite(acionar_k1_pin, HIGH);
+      doOnce = false;
+    }
 }
