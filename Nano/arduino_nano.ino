e3bff2112310d2d9534f37fb6c1990746acf9a2e
lucas-bernardino <lucasgber@gmail.com>
2025-02-17 11:02:30 -0300

    Change bluetooth to serial on arduino nano code

--- Nano/arduino_nano.ino
+++ Nano/arduino_nano.ino
@@ -9,21 +9,15 @@ int thermoCS = 3;
 int thermoCLK = 2; // sck
 
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoD0);
-SoftwareSerial bt(RX, TX);
 
 void setup() {
   Serial.begin(9600);
-  bt.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
   float temp = thermocouple.readCelsius();
-
-  //Serial.println(temp);
-  bt.println(temp);
+  Serial.println(temp);
 
   delay(200);
 }
-
